5b32f3f1ff123f85d5c16cfa4fdde3fa4dc55891
Wesley Bernard <wesley.bernard64@gmail.com>
2022-03-29 15:21:20 -0400

    constants and variables and scoping I guess, but we already know about scoping

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,22 @@ fn main() {
     const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
 
     // the mut keyword before a variable allows it to be mutable, variables are immutable if declared without this keyword.
-    let mut x = 5;
+    let x = 5;
     println!("The value of x is: {}", x);
-    x = 6;
-    println!("The value of x is: {}", x);
+    // x = 6;
+    // println!("The value of x is: {}", x);
+
+    //redcclaring a immutable variable is known as shadowing, this allows us to use the same variable name while
+    //ensuring that the new value is also immutable going forward.
+    let x = x + 1;
+
+    {
+        //Shadowing obeys scoping rules, so a variable shadowed in a function will have it's old value outside of the
+        //function.
+        let x = x * 2;
+        println!("The value of the inner x is: {}", x);
+    }
+
+    println!("The value of the outer x is {}", x);
+
 }

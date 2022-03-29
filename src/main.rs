ebc60f9f94e74b6dd0935017aed22a995cdd00de
Wesley Bernard <wesley.bernard64@gmail.com>
2022-03-29 15:00:28 -0400

    constants and variables

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 fn main() {
-    let x = 5;
+    //constants are always immutable.
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
+    // the mut keyword before a variable allows it to be mutable, variables are immutable if declared without this keyword.
+    let mut x = 5;
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
